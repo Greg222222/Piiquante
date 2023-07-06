e139759967751ee51d492b7f26628216692a68f2
Greg <greggire@hotmail.fr>
2023-07-06 15:53:39 +0200

    add signup and login

--- Backend/app.js
+++ Backend/app.js
@@ -1,24 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user')
+
+mongoose.connect('mongodb+srv://greggire:CxztcikHrnl7PCaz@cluster0.vtjunqf.mongodb.net/ ',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !')); 
+
 
 const app = express();
+app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); //Permet d'accéder à notre API depuis n'importe quelle origine ( '*' ) à changer une fois déployée
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Permet d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next(); 
+})
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+app.use('/api/auth', userRoutes)
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
 
 module.exports = app;
